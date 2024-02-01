3940e9e8cf6127fbb46501b553b406d4497947ca
Karumaidoi <mainahmwangi12@gmail.com>
2024-02-02 00:34:22 +0700

    code refactor

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const app = require("./app");
 // Configuring our environment variables
 dotenv.config({ path: "./env" });
 
-// Setting up the server and a PORT configurations
-// Provide a fallback PORT incase it not identified in our env
+/*
+Setting up the server and a PORT configurations
+Provide a fallback PORT incase it not identified in our env
+ */
 const port = process.env.PORT || 4400;
-
-// Create the server to serve our requests
-// To log our server we can use a package such as Morgan
+/*
+Create the server to serve our requests
+To log our server we can use a package such as Morgan
+*/
 const server = app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
