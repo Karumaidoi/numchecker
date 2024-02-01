10019efcbbf0b11582e6149672ba883f3055b4b5
Karumaidoi <mainahmwangi12@gmail.com>
2024-02-01 22:18:55 +0700

    configured the express router with the get method

--- routes/numberRoute.js
+++ routes/numberRoute.js
@@ -6,4 +6,8 @@ const express = require("express");
 // Configiring our express router
 const router = express.Router();
 
-// Adding
+/*
+Adding HTTP Methods on the router -> [GET,POST]
+Inside the GET Method -> Pass the controller method that will be executed when this endpoint is fired
+*/
+router.route("/").get();
